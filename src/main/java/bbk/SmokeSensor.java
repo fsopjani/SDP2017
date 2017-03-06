57ae243661f25e7cf6dba89c338c66f47e835c68
fsopjani <fatos@Fatoss-MacBook-Pro.local>
2017-03-06 00:17:57 +0000

    some more logic in it, get stragey pattern now in place

--- src/main/java/bbk/SmokeSensor.java
+++ src/main/java/bbk/SmokeSensor.java
@@ -1,26 +1,48 @@
 package bbk;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class SmokeSensor implements Sensor {
+  private String location;
+  private String sensorType;
+  private int batteryPercentage;
+  private int pollCount = 0;
+
+  public SmokeSensor(String location, String sensorType) {
+    this.location = location;
+    this.sensorType = sensorType;
+    this.batteryPercentage = 100;
+  }
+
   @Override
   public boolean isTriggered() {
+    pollCount++;
+    batteryPercentage -= 0.20 * batteryPercentage;
+
+    if (100 / pollCount <= 10 && batteryPercentage > 0) {
+      pollCount = 0;
+      return true;
+    }
+
+    if (batteryPercentage <= 0) {
+      System.out.println("Automatic battery charge, why not!");
+      batteryPercentage = 100;
+    }
+    System.out.println("poll % : " + 100 / pollCount);
+    System.out.println(pollCount + " " + batteryPercentage);
     return false;
   }
 
   @Override
   public String getLocation() {
-    return null;
+    return location;
   }
 
   @Override
   public String getSensorType() {
-    return null;
+    return sensorType;
   }
 
   @Override
-  public double getBatteryPercentage() {
-    return -1;
+  public int getBatteryPercentage() {
+    return batteryPercentage;
   }
 }
