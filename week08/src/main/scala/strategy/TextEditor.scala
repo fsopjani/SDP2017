5e26e2c4592848371f3ee9be0fc0b1fd2fb411f2
fsopjani <fatos@Fatoss-MacBook-Pro.local>
2017-04-11 22:03:27 +0100

    strategy

--- week08/src/main/scala/strategy/TextEditor.scala
+++ week08/src/main/scala/strategy/TextEditor.scala
@@ -3,7 +3,7 @@ package strategy
 // TODO
 
 case class TextEditor(formatter: TextFormatter) {
-  def publishText(s: String) = formatter match {
+  def publishText(s: String): Unit = formatter match {
     case _: CapTextFormatter => formatter.format(s)
     case _: LowerTextFormatter => formatter.format(s)
   }
