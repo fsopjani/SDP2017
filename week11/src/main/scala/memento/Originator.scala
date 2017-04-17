050d801ae7e50ef168d8354e04133f452c74d510
Fatos Sopjani <fatos@Fatoss-MacBook-Pro.local>
2017-04-17 15:40:40 +0100

    memento, not so sure about it, couple if thingsare not quite there

--- week11/src/main/scala/memento/Originator.scala
+++ week11/src/main/scala/memento/Originator.scala
@@ -6,19 +6,36 @@ case class Originator(
   var careTaker: CareTaker
 ) {
 
+  private val momento: Memento = new Memento(x,y)
+
   private var lastUndoSavepoint: String = _
 
   createSavepoint("INITIAL")
 
-  def createSavepoint(savepointName: String): Unit = ???
+  def createSavepoint(savepointName: String): Unit = {
+    println(s"Saving state... $savepointName")
+    lastUndoSavepoint = savepointName
+    careTaker.saveMemento(momento, savepointName)
+  }
 
-  def undo(): Unit = ???
+  def undo(): Unit = {
+    careTaker.getMemento(lastUndoSavepoint)
+  }
 
-  def undo(savepointName: String): Unit = ???
+  def undo(savepointName: String): Unit = {
+    println(s"Undo at ...$savepointName")
+    careTaker.getMemento(savepointName)
+  }
 
-  def undoAll(): Unit = ???
+  def undoAll(): Unit = {
+    println("Clearing all save points...")
 
-  private def setOriginatorState(savepointName: String): Unit = ???
+    careTaker.clearSavepoints()
+  }
+
+  private def setOriginatorState(savepointName: String): Unit = {
+    careTaker.getMemento(savepointName) //not too sure
+  }
 
   override def toString(): String = "X: " + x + ", Y: " + y
 }
